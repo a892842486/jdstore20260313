6b17371f82a5ad67a30c5a115b4bf48e3ea8fd25
Bamu <a892842486@github.com>
2026-03-23 11:25:52 +0800

    feat(route): add admin products routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "welcome#index"
+
+  namespace :admin do
+    resources :products
+  end
 end
