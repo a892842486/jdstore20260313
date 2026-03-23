7fc35f50512b5e57830ba3582d429403a2d5bdc1
Bamu <a892842486@github.com>
2026-03-23 11:27:10 +0800

    feat(model): create Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_20_084603) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_23_032641) do
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "description"
+    t.integer "price"
+    t.integer "quantity"
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
