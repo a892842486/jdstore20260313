f00a43ad54cded057ec6025e71748e19fbee7777
Bamu <a892842486@github.com>
2026-03-23 14:09:02 +0800

    chore(seed): add default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.find_or_create_by!(email: "123@123") do |user|
+  user.password = "123123"
+  user.password_confirmation = "123123"
+  user.is_admin = true
+end
