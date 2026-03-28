ccf2753fc4e025249173410fe8e8b0517d0b777a
Bamu <a892842486@github.com>
2026-03-28 10:58:56 +0800

    feat(cart): add checkout entry point from cart page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   resources :carts, only: [:index] do
     collection do
       delete :clean
+      post :checkout
     end
   end
 
@@ -34,5 +35,5 @@ Rails.application.routes.draw do
    resources :cart_items
 
 
-   
+
 end
