402e3ba014212725f0cedd30a0694aadef4f4e57
Bamu <a892842486@github.com>
2026-04-09 16:32:22 +0800

    refactor(admin/orders): integrate AASM events

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,37 +2,38 @@ class Admin::OrdersController < ApplicationController
   layout "admin"
   before_action :authenticate_user!
   before_action :admin_required
+  before_action :set_order, only: [:show, :ship, :shipped, :cancel, :return]
 
   def index
     @orders = Order.order(id: :desc)
   end
 
   def show
-    @order = Order.find(params[:id])
-    @product_lists = @order.product_lists
   end
 
   def ship
-    @order = Order.find(params[:id])
     @order.ship!
-    redirect_back(fallback_location: admin_orders_path)
+    redirect_back fallback_location: admin_orders_path
   end
 
   def shipped
-    @order = Order.find(params[:id])
     @order.deliver!
-    redirect_back(fallback_location: admin_orders_path)
+    redirect_back fallback_location: admin_orders_path
   end
 
   def cancel
-    @order = Order.find(params[:id])
     @order.cancel_order!
-    redirect_back(fallback_location: admin_orders_path)
+    redirect_back fallback_location: admin_orders_path
   end
 
   def return
-    @order = Order.find(params[:id])
     @order.return_good!
-    redirect_back(fallback_location: admin_orders_path)
+    redirect_back fallback_location: admin_orders_path
+  end
+
+  private
+
+  def set_order
+    @order = Order.find_by!(token: params[:id])
   end
 end
